9a9423cffc190a2531f916835e26a711821e3d25
Fierra <fierra@demc.store>
2024-07-25 12:55:19 +0800

    deleting useless stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,4 @@
 // @ts-check
-
-
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
 // There are various equivalent ways to declare your Docusaurus config.
